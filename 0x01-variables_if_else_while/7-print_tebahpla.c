293d120f59c83e20b647329ee88d6bce898edb03
Terilah <terilamax1@gmail.com>
2022-10-14 18:07:59 -0700

    t7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,13 +7,13 @@
 */
 int main(void)
 {
-		int a;
+	int a;
 
 
-			for (a = 'z'; a >= 'a'; a--)
-					{
-								putchar(a);
-									}
-				putchar('\n');
-					return (0);
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
 }
