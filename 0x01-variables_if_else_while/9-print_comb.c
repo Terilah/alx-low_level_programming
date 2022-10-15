ca904436229be529e4edf373cd673925cbc7fbc3
Terilah <terilamax1@gmail.com>
2022-10-14 18:15:50 -0700

    t9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
 * main - Entry point
 *Description: prints numbers
@@ -7,32 +7,19 @@
 */
 int main(void)
 {
-
 	int i;
 
-
-	
 	for (i = '0'; i <= '9' ; i++)
-
 	{
-
 		putchar(i);
-
-
 		
 		if (i != '9')
-
 		{
-
 			putchar(',');
-
-		
-		putchar(' ');
+			putchar(' ');
 		}
 	}
-
 	
 	putchar('\n');
-
 	return (0);
 }
