e1899c5f5c07605834faee7b7be5a770adbe4b25
Terilah <terilamax1@gmail.com>
2022-10-14 17:59:27 -0700

    t6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,11 @@
 */
 int main(void)
 {
-		int a;
+	int a;
 
-			for (a = 0; a < 10; a++)
-					{
-								printf("%d", a);
-									}
-				putchar('\n');
-					return (0);
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');					}
+	putchar('\n');
+	return (0);
 }
