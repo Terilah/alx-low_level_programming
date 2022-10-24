3e2f2d2a1b48a048cde387a3bb15aa1ceaea0f2a
Terilah <terilamax1@gmail.com>
2022-10-24 11:09:00 -0700

    t0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*reset_to_98- it reset to 98
+*@n: checked
+*Return: nothing
+*/
+<<<<<<< HEAD
+void reset_to_98(int *n);
+=======
+void reset_to_98(int *n)
+>>>>>>> 65fcb901d6027ccc57c4bfb39c37bb3ddbedb91d
+{
+	int *ptr;
+
+	ptr = n;
+
+	*ptr = 98;
+}
